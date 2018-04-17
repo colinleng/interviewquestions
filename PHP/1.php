1ce42721f69c3ab0398615ec74e3c2650552e901
TranceLin <trancelin@cmlove.cc>
2018-04-17 17:56:34 +0800

    v0.2

---
+++ PHP/1.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * 使用一个辅助栈，O(1)复杂度求出栈中的最小数
+ * @hack 类中通过数组来模拟堆栈
+ *
+ * @author laiwenhui
+ */
+class strack{
+
+    /**
+     * 数据栈，存储栈数据；
+     *
+     * @var array
+     */
+    private $_arrData = array();
+    /**
+     * 辅助栈，存储数据组栈中每层的最下值信息；
+     *
+     * @var array
+     */
+    private $_arrMin = array();
+    /**
+     * 栈顶所在单元
+     *
+     * @var int
+     */
+    private $_top=-1;
+    /**
+     * 出栈
+     * @return bool|int
+     */
+    public function pop(){
+        if ($this->_top === -1){
+            return false;
+        }
+        array_pop($this->_arrMin);
+        $this->_top--;
+        return array_pop($this->_arrData);
+    }
+    /**
+     * 入栈
+     * @param int $element
+     * @return bool
+     */
+    public function push($element){
+        $element = intval($element);
+        //如果栈为空，直接入栈
+        if ($this->_top === -1){
+            array_push($this->_arrData, $element);
+            array_push($this->_arrMin, $element);
+            $this->_top++;
+            return true;
+        }
+        //不为空，判断入栈的值是否比最小栈栈顶小
+        $min = $this->_arrMin[$this->_top];
+        //比较求出最小值
+        $currentMin = $element < $min ? $element : $min;
+        //当前栈中最小值入栈
+        array_push($this->_arrMin, $currentMin);
+        //数据入栈
+        array_push($this->_arrData, $element);
+        $this->_top++;
+
+        return true;
+    }
+    /**
+     * 求当前栈空间的最小值
+     * @return bool|int
+     */
+    public function min(){
+        if ($this->_top === -1){
+            return false;
+        }
+        return $this->_arrMin[$this->_top];
+    }
+}
+?>
---
+++ PHP/README.md
@@ -0,0 +1,6 @@
+## PHP代码内容
+
+		本目录下面基本介绍遇到的和PHP代码相关的内容，仅个人见解，如果当中有错误内容请您进行指正。
+		
+	
+* 1、[PHP实现堆栈](https://github.com/colinleng/interviewquestions/blob/master/%E5%86%85%E5%AD%98/1.php)
